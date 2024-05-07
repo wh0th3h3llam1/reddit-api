df49bb122d606fe75450d170f1cf3d9edf81fe96
wh0th3h3llam1 <wh0am1.7548@gmail.com>
2024-05-08 02:54:47 +0530

    updated admin

--- subreddit/admin/subreddit_admin.py
+++ subreddit/admin/subreddit_admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin, messages
+from django.db.models import BooleanField, Case, F, QuerySet, When
 from django.utils.html import format_html
 from django.utils.translation import ngettext
 
@@ -22,6 +23,7 @@ class SubredditAdmin(admin.ModelAdmin):
     list_display = ("name", "display_name", "owner", "nsfw")
     list_filter = ("owner", "nsfw")
     search_fields = ("name", "display_name")
+    search_help_text = "Search via Name or Display name"
     inlines = (SubredditLinkInline,)
     readonly_fields = ("about",)
 
@@ -32,9 +34,64 @@ class ModeratorAdmin(admin.ModelAdmin):
 
 
 class SubredditUserAdmin(admin.ModelAdmin):
-    list_display = ("user", "subreddit", "created")
-    list_filter = ("subreddit",)
+    list_display = ("user", "subreddit", "is_user_a_moderator", "created")
+    list_filter = ("subreddit",)  # ("is_moderator", IsModeratorAdminFilter))
     search_fields = ("user__username",)
+    search_help_text = "Search via Username"
+    actions = ("ban_users",)
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(
+            moderator_user_id=F("subreddit__moderators__user__id"),
+        ).annotate(
+            is_mod=Case(
+                When(
+                    user_id__in=F("moderator_user_id"),
+                    then=True,
+                ),
+                default=False,
+                output_field=BooleanField(),
+            )
+        )
+
+        return queryset
+
+    def is_user_a_moderator(self, instance: SubredditUser) -> bool:
+        return instance.is_moderator
+
+    @admin.action(description="Ban Selected Users")
+    def ban_users(self, request, queryset: QuerySet["SubredditUser"]):
+        # Must verify that no mods are being banned
+        if queryset.filter(is_mod=True).exists():
+            self.message_user(
+                request,
+                "Some users are Moderators for their respective subreddits. They cannot be banned",
+                messages.ERROR,
+                fail_silently=False,
+            )
+        else:
+            updated = self._ban_users(queryset)
+            self.message_user(
+                request,
+                ngettext(
+                    singular=f"{updated[0]} user banned",
+                    plural=f"{updated[0]} users banned",
+                    number=updated,
+                ),
+                messages.SUCCESS,
+            )
+
+    def _ban_users(self, queryset: QuerySet["SubredditUser"]):
+        banned_users = [
+            BannedUser(subreddit=obj.subreddit, user=obj.user)
+            for obj in queryset
+        ]
+        created = BannedUser.objects.bulk_create(banned_users)
+        return queryset.delete()
+
+    is_user_a_moderator.short_description = "Is Moderator?"
+    is_user_a_moderator.boolean = True
 
 
 class SubredditLinkAdmin(admin.ModelAdmin):
@@ -66,7 +123,7 @@ class BannedUserAdmin(admin.ModelAdmin):
 
     @admin.action(description="Unban selected Users")
     def unban_users(self, request, queryset):
-        updated = queryset.delete()
+        updated = queryset.delete()[0]
         self.message_user(
             request,
             ngettext(
@@ -95,9 +152,9 @@ class BannedUserAdmin(admin.ModelAdmin):
         self.message_user(
             request,
             ngettext(
-                singular=f"{updated} user unbanned",
-                plural=f"{updated} users unbanned",
-                number=updated,
+                singular=f"{updated[0]} user unbanned",
+                plural=f"{updated[0]} users unbanned",
+                number=updated[0],
             ),
             messages.SUCCESS,
         )
